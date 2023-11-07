5d1a9fa1a201291bfec5dc7a79e23b49e1819da9
sajeeb23 <sajeebsharma24@gmail.com>
2023-11-08 02:38:03 +0600

    404 page created

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../pages/Home/Home";
 import AddJob from "../pages/Jobs/AddJob";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
 
 
 
@@ -9,6 +10,7 @@ const router = createBrowserRouter([
     {
       path: "/",
       element: <Main></Main>,
+      errorElement: <ErrorPage></ErrorPage>,
       children: [{
         path: "/",
         element: <Home></Home>
